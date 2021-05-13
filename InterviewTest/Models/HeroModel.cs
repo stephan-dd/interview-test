e81b3265b7c8b691e5b2a11acf8b2e52275e7355
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2021-05-14 01:53:41 +0200

    Extracted hero properties to a model

---
+++ InterviewTest/Models/HeroModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewTest.Models
+{
+    public class HeroModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Power { get; set; }
+        public List<KeyValuePair<string, int>> Stats { get; set; }
+    }
+}
