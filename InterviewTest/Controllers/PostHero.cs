909fa607cbecfa8ad4641d2a591c17b4f78ef413
Malesela Seabi <seabiali@gmail.com>
2021-06-15 15:30:06 +0200

    Post method and evolve

---
+++ InterviewTest/Controllers/PostHero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewTest.Controllers
+{
+    public class PostHero
+    {
+        public string name { get; set; }
+        public string action = "none";
+    }
+}
