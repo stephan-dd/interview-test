d3327fb888ace489a6a87d66dac8b403edf5dc3a
Malesela Seabi <seabiali@gmail.com>
2021-06-15 14:42:19 +0200

    Implement evolve method

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -12,7 +12,14 @@ namespace InterviewTest.Controllers
         public List<KeyValuePair<string, int>> stats {get;set;}
         public void evolve(int statIncrease = 5)
         {
-            
+            List<KeyValuePair<string, int>> statsUpdated = new List<KeyValuePair<string, int>>(); 
+            stats.ForEach(delegate(KeyValuePair<string, int> stat){
+                string key = stat.Key;
+                decimal statVal = (decimal)stat.Value;
+                int statValHalf = (int)Math.Truncate(statVal / 2);
+                statsUpdated.Add(new KeyValuePair<string, int>(key, statValHalf));
+            });
+            stats = statsUpdated;
         }
     }
 }
