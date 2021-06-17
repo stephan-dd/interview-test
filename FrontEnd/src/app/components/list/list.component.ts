a5513b78aa617953e266efe9fcfe52db12dc7d13
Malesela Seabi <seabiali@gmail.com>
2021-06-17 06:14:35 +0200

    table style and message stats fix

--- FrontEnd/src/app/components/list/list.component.ts
+++ FrontEnd/src/app/components/list/list.component.ts
@@ -5,7 +5,7 @@ import { ApiService } from '../../services/api.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
 
