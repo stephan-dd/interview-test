4e2c2b8d65e463d2335885976cc7a347c3cc2620
Heinrich Heine <h.heine@live.co.za>
2021-08-23 23:49:48 +0200

    -

---
+++ InterviewTest/Controllers/Heroes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewTest.Controllers
+{
+    public class Heroes
+    {
+       
+        public static IHero[] GetHeroes()
+        {
+            return new Hero[] {
+               new Hero()
+               {
+                   Name= "Hulk",
+                   Power="Strength from gamma radiation",
+                   Stats=
+                   new List<KeyValuePair<string, int>>()
+                   {
+                       new KeyValuePair<string, int>( "strength", 5000 ),
+                       new KeyValuePair<string, int>( "intelligence", 50),
+                       new KeyValuePair<string, int>( "stamina", 2500 )
+                   }
+
+               },
+               new Hero()
+               {
+                   Name= "Iron man",
+                   Power="Possesses powered armor that gives him superhuman strength and durability, flight, and an array of weapons",
+                   Stats=
+                   new List<KeyValuePair<string, int>>()
+                   {
+                       new KeyValuePair<string, int>( "strength", 1500 ),
+                       new KeyValuePair<string, int>( "intelligence", 3000),
+                       new KeyValuePair<string, int>( "stamina", 2000 )
+                   }
+               },
+               new Hero()
+               {
+                   Name= "Spiderman",
+                   Power="Superhuman strength, speed, reflexes, agility, coordination and balance",
+                   Stats=
+                   new List<KeyValuePair<string, int>>()
+                   {
+                       new KeyValuePair<string, int>( "strength", 1000 ),
+                       new KeyValuePair<string, int>( "intelligence", 500),
+                       new KeyValuePair<string, int>( "stamina", 3000 )
+                   }
+               }
+            };
+        }
+    }
+}
--- InterviewTest/Controllers/HeroesController.cs
+++ InterviewTest/Controllers/HeroesController.cs
@@ -11,65 +11,26 @@ namespace InterviewTest.Controllers
     [ApiController]
     public class HeroesController : ControllerBase
     {
-        private IHero[] heroes = new Hero[] {
-               new Hero()
-               {
-                   Name= "Hulk",
-                   Power="Strength from gamma radiation",
-                   Stats=
-                   new List<KeyValuePair<string, int>>()
-                   {
-                       new KeyValuePair<string, int>( "strength", 5000 ),
-                       new KeyValuePair<string, int>( "intelligence", 50),
-                       new KeyValuePair<string, int>( "stamina", 2500 )
-                   }
-                   
-               },
-               new Hero()
-               {
-                   Name= "Iron man",
-                   Power="Possesses powered armor that gives him superhuman strength and durability, flight, and an array of weapons",
-                   Stats=
-                   new List<KeyValuePair<string, int>>()
-                   {
-                       new KeyValuePair<string, int>( "strength", 1500 ),
-                       new KeyValuePair<string, int>( "intelligence", 3000),
-                       new KeyValuePair<string, int>( "stamina", 2000 )
-                   }
-               },
-               new Hero()
-               {
-                   Name= "Spiderman",
-                   Power="Superhuman strength, speed, reflexes, agility, coordination and balance",
-                   Stats=
-                   new List<KeyValuePair<string, int>>()
-                   {
-                       new KeyValuePair<string, int>( "strength", 1000 ),
-                       new KeyValuePair<string, int>( "intelligence", 500),
-                       new KeyValuePair<string, int>( "stamina", 3000 )
-                   }
-               }
-            };
-
+      
         // GET: api/Heroes
         [HttpGet]
         public IEnumerable<IHero> Get()
         {
-            return this.heroes;
+            return Heroes.GetHeroes();
         }
 
         // GET: api/Heroes/5
         [HttpGet("{id}", Name = "Get")]
         public IHero Get(int id)
         {
-            return this.heroes.FirstOrDefault();
+            return Heroes.GetHeroes().FirstOrDefault();
         }
 
         // POST: api/Heroes
         [HttpPost("{name}/{act=none}/{statIncrease:int=1}")]
         public IHero Post(string name, string act, int statIncrease)
         {
-            IHero hero = heroes.Where(h => h.Name == name).FirstOrDefault();
+            IHero hero = Heroes.GetHeroes().Where(h => h.Name == name).FirstOrDefault();
             if (hero != null)
             {
                 switch (act.ToLower())
