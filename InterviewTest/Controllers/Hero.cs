0396e81fd10f16411bf5875ef84434b4254cc93d
NombusoPro <nombusojuliet@gmail.com>
2021-10-25 09:44:28 +0200

    Evolve method interface and implementation

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,20 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero : IHero
     {
         public string name { get; set; }
         public string power { get; set; }
         public List<KeyValuePair<string, int>> stats {get;set;}
-        public void evolve(int statIncrease = 5)
+        public void evolve()
         {
-            
+            List<KeyValuePair<string, int>> copyStats = new List<KeyValuePair<string, int>>();
+            foreach (var stat in stats)
+            {
+                int statVal = stat.Value + stat.Value / 2;
+                copyStats.Add(new KeyValuePair<string, int>(stat.Key, statVal));
+            }
+            stats = copyStats;
         }
     }
 }
