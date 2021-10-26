4a28fea8680d64fd39b60f109fb089c7fdee35f6
Michal <44830778+TaxableLemon@users.noreply.github.com>
2021-10-26 13:19:34 +0200

    Michal Priem Interview Test Changes

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,21 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero : IHero
     {
         public string name { get; set; }
         public string power { get; set; }
-        public List<KeyValuePair<string, int>> stats {get;set;}
-        public void evolve(int statIncrease = 5)
+        public string action { get; set; }
+        public List<KeyValuePair<string, int>> stats { get; set; }
+        public void evolve(double statIncrease = 5.0)
         {
-            
-        }
+            foreach (KeyValuePair<string, int> stat in stats.ToList())
+                if (stats.Contains(stat))
+                {
+                    int i = stats.IndexOf(stat);
+                    KeyValuePair<string, int> newValue = new KeyValuePair<string, int>(stat.Key, (int)(stat.Value + stat.Value * statIncrease));
+                    stats[i] = newValue;
+                }
+        } 
     }
 }
