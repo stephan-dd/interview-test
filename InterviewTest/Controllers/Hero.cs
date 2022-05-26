99579bb30ec828d57cf81d94904760373298363b
Tumelo <tumelo.mokhwathi@gmail.com>
2022-05-26 15:38:00 +0200

    Updated

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -17,7 +17,6 @@ namespace InterviewTest.Controllers
     {
         public string name { get; set; }
         public string power { get; set; }
-        public string action { get; set; }
         public List<KeyValuePair<string, int>> stats { get; set; }
         public void evolve(int statIncrease = 5)
         {
