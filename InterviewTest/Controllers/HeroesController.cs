a55d5f74bb89a8426c8291c95ed0717c0f6be17c
leonardo99551@gmail.com <Franco.leonardo99551@gmail.com>
2022-07-08 09:56:24 +0200

    testing... evolve body parse fail

--- InterviewTest/Controllers/HeroesController.cs
+++ InterviewTest/Controllers/HeroesController.cs
@@ -14,20 +14,22 @@ namespace InterviewTest.Controllers
         private Hero[] heroes = new Hero[] {
                new Hero()
                {
-                   name= "Hulk",
-                   power="Strength from gamma radiation",
-                   stats=
-                   new List<KeyValuePair<string, int>>()
-                   {
-                       new KeyValuePair<string, int>( "strength", 5000 ),
-                       new KeyValuePair<string, int>( "intelligence", 50),
-                       new KeyValuePair<string, int>( "stamina", 2500 )
-                   }
+                   name = "Hulk",
+                   power ="Strength from gamma radiation",
+                   stats =
+                       new List<KeyValuePair<string, int>>()
+                       {
+                           new KeyValuePair<string, int>( "strength", 5000 ),
+                           new KeyValuePair<string, int>( "intelligence", 50),
+                           new KeyValuePair<string, int>( "stamina", 2500 )
+                       }
                }
             };
 
+
         // GET: api/Heroes
         [HttpGet]
+        [Route("Get")]
         public IEnumerable<Hero> Get()
         {
             return this.heroes;
@@ -42,8 +44,31 @@ namespace InterviewTest.Controllers
 
         // POST: api/Heroes
         [HttpPost]
+        [Route("Post")]
         public void Post([FromBody] string value)
         {
+            //only act on evolve
+            if(value == "evolve")
+            {
+                //stats KeyPair holder
+                Dictionary<string, int> newKP = new Dictionary<string, int>();
+
+                for (int i = 0; i < heroes.Count(); i++)
+                {
+                    //get key value pairs
+                    List<KeyValuePair<string,int>> hStats = heroes[i].stats;
+
+                    //evolve each stat on hero
+                    for(int j = 0; j < hStats.Count(); j++)
+                    {
+                        Hero hero = new Hero(); //inject
+                        newKP.Add(hStats[j].Key, hero.evolve(hStats[j].Value));
+                    }
+
+                    //convert back
+                    heroes[i].stats = newKP.ToList();
+                }
+            }
         }
 
         // PUT: api/Heroes/5
--- InterviewTest/Startup.cs
+++ InterviewTest/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InterviewTest.Controllers;
+using InterviewTest.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace InterviewTest
 {
@@ -36,7 +39,12 @@ namespace InterviewTest
                 });
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //.net 2.0 (json serialization)
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            .AddJsonOptions(options =>
+            options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            services.AddScoped<IHero, Hero>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
