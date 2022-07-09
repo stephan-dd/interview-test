34f04c87a7cb7d3d0addf581da05d78db5240d98
Choene <cchokoe@yahoo.com>
2022-07-09 21:48:50 +0200

    C# API - Refactor the Hero class to implement an interface of IHero.

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero : IHero
     {
         public string name { get; set; }
         public string power { get; set; }
-        public List<KeyValuePair<string, int>> stats {get;set;}
+        public List<KeyValuePair<string, int>> stats { get; set; }
         public void evolve(int statIncrease = 5)
         {
-            
+
         }
     }
 }
