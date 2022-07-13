4559b05ce8e845a5e08d41074baeb96af3116f0f
Moraswi Maja <moraswi@akhile.co.za>
2022-07-13 23:55:27 +0200

    - Added IHero interface - Implemented IHero interface in Hero class - Implemented IhHero interface in HeroesController
    v1

---
+++ InterviewTest/Controllers/IHero.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewTest.Controllers
+{
+    public interface IHero
+    {
+        string Name { get; set; }
+        string Power { get; set; }  
+        List<KeyValuePair<string, int>> Stats { get; set; }
+        void Evolve(int statIncrease);
+    }
+}
