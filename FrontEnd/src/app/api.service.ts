c91bc7c0b9294062c4e48ebad9c37d149f6fa8bc
Shahil Sunder <shahilsunder@dimensiondata.com>
2022-07-15 21:41:49 +0200

    Added API call and Hero Interface

--- FrontEnd/src/app/api.service.ts
+++ FrontEnd/src/app/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>("http://localhost:4201/api/heroes");
+  }
+
 }
+interface Hero { 
+  name: string, 
+  power: string, 
+  stats: [
+    { key: string, value: number }, 
+    { key: string, value: number }, 
+    { key: string, value: number }] 
+  }
