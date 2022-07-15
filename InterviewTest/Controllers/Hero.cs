098d8cd94cd714aef41764d0e8a0544a7131aba9
Zhilong-coding <zhilong.k.liang@gmail.com>
2022-07-15 17:01:11 +0200

    Completed Task
    Completed API (C#) method
    
    Angular:
    - New Service API
    - New List component, View, SCSS
    - Display Hero stats to view in table form.
    
    CSS:
    - Added heading
    - Random Colours are picked and applied to the table.

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,23 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero : IHero
     {
         public string name { get; set; }
         public string power { get; set; }
-        public List<KeyValuePair<string, int>> stats {get;set;}
+        public List<KeyValuePair<string, int>> stats { get; set; }
         public void evolve(int statIncrease = 5)
         {
-            
+            List<KeyValuePair<string, int>> tempValue = new List<KeyValuePair<string, int>>();
+
+            foreach(KeyValuePair<string, int> item in stats.ToList())
+            {
+                //Increment each stat value with a multiple of half of the original value
+                tempValue.Add(new KeyValuePair<string, int>(item.Key, (int)(item.Value+(item.Value/2))));
+            }
+
+            //assign the result to stats
+            stats = tempValue;
         }
     }
 }
