9badd7777ccf205516056f9c3ec92d81e227511b
Shahil Sunder <shahilsunder@dimensiondata.com>
2022-07-15 18:30:21 +0200

    Refactored Hero to have IHero Interface

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,23 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public interface IHero
+    {
+        string name { get; set; }
+        string power { get; set; }
+        List<KeyValuePair<string, int>> stats { get; set; }
+
+        void evolve(int statIncrease = 5);
+    }
+
+    public class Hero : IHero
     {
         public string name { get; set; }
         public string power { get; set; }
-        public List<KeyValuePair<string, int>> stats {get;set;}
+        public List<KeyValuePair<string, int>> stats { get; set; }
         public void evolve(int statIncrease = 5)
         {
-            
+
         }
     }
 }
