317bc3f0eb1f3562f7b4626247af55da40df59cb
Shahil Sunder <shahilsunder@dimensiondata.com>
2022-07-15 20:01:46 +0200

    Implemented evolve method

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
 
 namespace InterviewTest.Controllers
 {
@@ -21,7 +20,9 @@ namespace InterviewTest.Controllers
         public List<KeyValuePair<string, int>> stats { get; set; }
         public void evolve(int statIncrease = 5)
         {
-
+            for (int i = 0; i < this.stats.Count; i++){
+                this.stats[i] = new KeyValuePair<string, int>(stats[i].Key, (int)(stats[i].Value + (0.5 * stats[i].Value)));
+            }
         }
     }
 }
