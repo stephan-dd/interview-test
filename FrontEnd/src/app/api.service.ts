773b2e6d32c6017ce721eba112fd8cf2036fea03
marvro <marv@mailbox.co.za>
2022-07-23 22:08:43 +0200

    1st commit test
    the initial run of the application for test run

---
+++ FrontEnd/src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  apiURL = 'http://localhost:4201';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getContacts(){
+    return this.httpClient.get(`${this.apiURL}/heroes`).toPromise;
+  }
+
+}
