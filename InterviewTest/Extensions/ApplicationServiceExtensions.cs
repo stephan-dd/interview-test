36f9612f1909010b962c48cc23d57223272f9a11
joedolamo <joedolamo@gmail.com>
2022-07-31 15:43:47 +0200

    Adding IHero interface and implementing extension method

---
+++ InterviewTest/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using InterviewTest.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewTest.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection service)
+        {
+            service.AddScoped<IHero, Hero>();
+            return service;
+        }
+    }
+}
