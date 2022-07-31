6e9634b628932c2fa09f8b0dff32b42d175a7add
joedolamo <joedolamo@gmail.com>
2022-07-31 15:24:14 +0200

    Updating the API so that it works on my environment

--- InterviewTest/Startup.cs
+++ InterviewTest/Startup.cs
@@ -31,12 +31,12 @@ namespace InterviewTest
                 {
                     builder.AllowAnyHeader()
                         .AllowAnyMethod()
-                        .AllowAnyOrigin()
+                        .SetIsOriginAllowed(origin => true)
                         .AllowCredentials();
                 });
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(x => x.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
