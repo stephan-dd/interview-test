017552c21520bf860f32ab8b8ead6c2b258b6639
joedolamo <joedolamo@gmail.com>
2022-07-31 16:00:24 +0200

    creating the evolve function

--- InterviewTest/Models/Hero.cs
+++ InterviewTest/Models/Hero.cs
@@ -12,7 +12,16 @@ namespace InterviewTest.Controllers
         public List<KeyValuePair<string, int>> stats { get; set; }
         public void evolve(int statIncrease = 5)
         {
+            var diction = new List<KeyValuePair<string, int>>();
+            foreach (KeyValuePair<string, int> kvp in stats)
+            {
+                var newVal = kvp.Value + kvp.Value * 1 / 2;
+                var newEntry = new KeyValuePair<string, int>(kvp.Key, newVal);
+                var removeOldItem = stats.Where(x => x.Key == kvp.Key).FirstOrDefault();
+                diction.Add(newEntry);
+            }
 
+            stats = diction;
         }
     }
 }
