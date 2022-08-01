0719743a9ad4a348a2ce8ac87305aa37c26d07ef
joedolamo <joedolamo@gmail.com>
2022-08-01 07:03:06 +0200

    Frontend changes

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
