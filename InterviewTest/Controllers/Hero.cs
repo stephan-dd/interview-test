2216f06838d19ce96150ef477fde735b954a3ac9
Mogomotsi91 <mo.ramesega@gmail.com>
2022-08-04 12:00:05 +0200

    testMogomotsi

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,65 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    interface Ihero 
+    {
+        string  name();
+        string power();  
+        List<string> list();
+        
+    }
+    public class Hero  : Ihero
     {
         public string name { get; set; }
         public string power { get; set; }
         public List<KeyValuePair<string, int>> stats {get;set;}
         public void evolve(int statIncrease = 5)
         {
+            foreach (KeyValuePair<string, int> stat in stats)
+            {
+                if (stat.Value > statIncrease)
+                {
+                    int half;
+                   int originalStats;
+                    
+                   originalStats = stat.Value;
+                    half =  originalStats / 2;
+                    
+                    statIncrease *= half;
+
+
+                    
+                }
             
+            }
+            
+        
+        }
+
+        public void post(Boolean evolv1e, bool action )
+        {
+            this.evolve(0);
+            var evolve = new evolve(stats);
+
+            
+        
+        }
+
+        public List<string> list()
+        {
+            throw new NotImplementedException();
+        }
+
+        string Ihero.name()
+        {
+            throw new NotImplementedException();
+        }
+
+        string Ihero.power()
+        {
+            throw new NotImplementedException();
         }
     }
+
+    
 }
