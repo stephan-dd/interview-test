f26a6eb4459f5398b8143664470ba315c704d4ec
Hlamulo Masonta <hlamulo.masonta@britehouse.co.za>
2022-09-21 15:42:13 +0200

    Commit Class Color

--- FrontEnd/src/app/list/list.component.ts
+++ FrontEnd/src/app/list/list.component.ts
@@ -1,39 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ApiService } from '../api.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { ApiService } from "../api.service";
+
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.scss"],
 })
 export class ListComponent implements OnInit {
-
   public contacts: any = [];
 
-  constructor(private service: ApiService) { }
+  constructor(private service: ApiService) {}
 
   ngOnInit() {
-
-    this.service.getContacts()
-    .subscribe( (contacts)=>{
-      this.contacts = contacts
-      console.log(contacts)
-    })
-   
+    this.service.getContacts().subscribe((contacts) => {
+      this.contacts = contacts;
+    });
   }
-  tableUpdated(){
-    this.service.postContacts()
-    .subscribe( (contacts)=>{
-      this.contacts = contacts
-    
-    })
-
-    
+  tableUpdated() {
+    this.service.postContacts().subscribe((contacts) => {
+      this.contacts = contacts;
+    });
   }
 
-
-  
-  
- 
+  getRandomColor2() {
+    var rendom = Math.floor(Math.random() * (Math.floor(3) - Math.ceil(0)) + Math.ceil(0));
+    var colors = ["class1", "class2", "class3", "class4"];
+    console.log(colors[rendom]);
+    return colors[rendom];
+  }
 }
